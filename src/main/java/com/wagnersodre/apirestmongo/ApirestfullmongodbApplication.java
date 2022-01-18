bb379e067bd7d458945719a500184ededc554da9
wagnerdsodre <wagnerdsodre@gmail.com>
2022-01-17 21:38:39 -0300

    Created project

---
+++ src/main/java/com/wagnersodre/apirestmongo/ApirestfullmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.wagnersodre.apirestmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfullmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfullmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagnersodre/apirestmongo/ApirestfullmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnersodre.apirestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfullmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
